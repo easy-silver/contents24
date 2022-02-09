c5b34de303999683994df596b5e464e1d34e2fff
Timo <developer.timo@gmail.com>
2022-02-09 17:50:03 +0900

    Refactor: Account 레포지토리 테스트 수정
    - DisplayName 어노테이션 추가
    - 테스트 케이스가 항상 성공하도록 코드 수정

--- src/test/java/homework/querydsl/contents24/repository/AccountRepositoryTest.java
+++ src/test/java/homework/querydsl/contents24/repository/AccountRepositoryTest.java
@@ -1,7 +1,12 @@
 package homework.querydsl.contents24.repository;
 
+import homework.querydsl.contents24.domain.account.Account;
+import homework.querydsl.contents24.domain.content.Content;
+import homework.querydsl.contents24.domain.employee.Employee;
 import homework.querydsl.contents24.domain.platform.Platform;
 import homework.querydsl.contents24.domain.account.AccountRepository;
+import homework.querydsl.contents24.domain.possession.Possession;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,15 +21,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class AccountRepositoryTest {
 
-    @Autowired
-    AccountRepository repository;
+    @Autowired AccountRepository repository;
     @Autowired EntityManager em;
 
     @Test
+    @DisplayName("플랫폼으로 전체 계정 조회")
     void findByPlatform() {
+        // 플랫폼 생성
+        Platform platform = Platform.builder()
+                .name("Inflearn")
+                .link("Inflearn.com")
+                .build();
+        em.persist(platform);
+
+        // 컨텐트 생성
+        Content content = Content.builder()
+                .platform(platform)
+                .name("Start Querydsl")
+                .build();
+        em.persist(content);
+
+        // 사원 생성
+        Employee employee = Employee.builder()
+                .employeeNo(111L)
+                .deptNo(999L)
+                .name("leejieun")
+                .build();
+        em.persist(employee);
+
+        // 계정 생성
+        Account account = Account.builder()
+                .accountId("jelee@snack24h.com")
+                .employee(employee)
+                .platform(platform)
+                .build();
+        em.persist(account);
+
+        // 보유 데이터 생성
+        Possession possession = Possession.builder()
+                .content(content)
+                .account(account)
+                .build();
+        em.persist(possession);
+
         // 인프런 조회
-        Platform platform = em.find(Platform.class, 1L);
-        List<Long> result = repository.findByPlatform(platform.getId());
+        Platform findPlatform = em.find(Platform.class, 1L);
+        List<Long> result = repository.findByPlatform(findPlatform.getId());
 
         for (Long id : result) {
             System.out.println("id = " + id);
