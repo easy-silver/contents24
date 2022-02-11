361b098c27d259c008eeaeab33b042ac7a848dc4
Timo <developer.timo@gmail.com>
2022-02-11 15:23:41 +0900

    Feat: RequestBody 유효성 검증 실패 예외에 대한 익셉션 핸들러 추가
    - MethodArgumentNotValidException 예외를 상태코드 400으로 반환하는 핸들러 추가

--- src/main/java/homework/querydsl/contents24/web/controller/exception/ExceptionController.java
+++ src/main/java/homework/querydsl/contents24/web/controller/exception/ExceptionController.java
@@ -3,6 +3,7 @@ package homework.querydsl.contents24.web.controller.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,15 +16,30 @@ import javax.persistence.EntityNotFoundException;
 @ControllerAdvice
 public class ExceptionController {
 
+
+    /**
+     * @RequestBody에 @Valid로 정의된 클래스에서 유효성 검증에 실패했을 경우 발생한다.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+
+        //유효성 검증 실패한 필드, 에러 메시지 정보
+        String errorField = e.getBindingResult().getFieldError().getField();
+        String defaultMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+
+        return new ResponseEntity<>(errorField + ": " + defaultMessage, HttpStatus.BAD_REQUEST);
+    }
+
     /**
      * PK로 조회 시 결과 값이 없을 경우 응답코드 404 반환
      * @param exception
      * @return
      */
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity entityNotFoundException(final RuntimeException exception) {
+    public ResponseEntity<String> entityNotFoundException(final RuntimeException exception) {
         log.info("error", exception);
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     /**
@@ -32,8 +48,8 @@ public class ExceptionController {
      * @return
      */
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity illegalArgumentException(final RuntimeException exception) {
+    public ResponseEntity<String> illegalArgumentException(final RuntimeException exception) {
         log.info("error", exception);
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
